31e066ba88e7c3f34fd3cc134e806af4546f5e86
iryna-ag <iryna.agafonov@gmail.com>
2022-04-29 23:51:14 -0400

    Fahrenheit link was added

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+export default function Temperature(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="Temperature">
+        <span className="temp">{Math.round(props.celsius)}</span>{" "}
+        <span className="units">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="Temperature">
+        <span className="temp">{Math.round(fahrenheit)}</span>{" "}
+        <span className="units">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
